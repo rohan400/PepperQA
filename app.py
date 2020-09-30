a36a6411ad8a9b9d2c38fd881898a5f41fcc781b
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-01 00:48:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def processRequest(req):
 '''if __name__ == '__main__':
     app.run()'''
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
 
 '''@app.route('/')
 def index():
