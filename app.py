2c6201142ca1c24440c40aa6d3e1117651cc0611
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-01 15:35:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,12 @@
-'''import numpy as np
-from flask import Flask, request, make_response
-import json
-import pickle
-from flask_cors import cross_origin
-import rs
+from flask import request
+from model import model
+import flask
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World'
-
-# geting and sending response to dialogflow
-@app.route('/webhook', methods=['POST'])
-@cross_origin()
-def webhook():
-
-    req = request.get_json(silent=True, force=True)
-
-    #print("Request:")
-    #print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    #print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def processRequest(req):
-
-    #sessionID=req.get('responseId')
-    result = req.get("queryResult")
-    #user_says=result.get("queryText")
-    #log.write_log(sessionID, "User Says: "+user_says)
-    parameters = result.get("parameters")
-    contexttemp=parameters.get("context")
-    questiontemp = parameters.get("question")
-    context = str(contexttemp)
-    question = str(questiontemp)
-    
-	 
-    intent = result.get("intent").get('displayName')
-    
-    if (intent=='QA - yes'):
-        #answer = 'Done'
-        answer = rs.predict(context, question)
-       
-        fulfillmentText= str(answer)
-        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
-        return {
-            "fulfillmentText": 'Please wait.....'
-        }
-    #else:
-    #    log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)'''
-
-# import flask dependencies
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
-'''@app.route('/')
 def index():
 
     if request.args:
@@ -83,10 +23,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))'''
-
-
-#if __name__ == '__main__':
-#    port = int(os.getenv('PORT', 5000))
-#    print("Starting app on port %d" % port)
-#    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
